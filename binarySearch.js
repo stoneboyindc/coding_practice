c0520e7bb9d18f8bffd050fbcb27ae61a75f1eaa
stoneboyindc <zzchang@gmail.com>
2019-04-07 18:40:06 -0400

    add square root function which is very similar to binary search on a sorted array

--- binarySearch.js
+++ binarySearch.js
@@ -3,7 +3,7 @@
 Implement a binary search function to search for a given item in a sorted array
 
 */
-function searchArray(input, target) {
+function searchArrayOld(input, target) {
     let lowerIdx = 0;
     let upperIdx = input.length - 1;
     let midIdx = Math.floor((upperIdx - lowerIdx) / 2) + lowerIdx;
@@ -24,6 +24,24 @@ function searchArray(input, target) {
     return -1;
 }
 
+
+function searchArray(input, target) {
+    let start = 0;
+    let end = input.length - 1;
+    let ans = 0;
+    while (start <= end) {
+        ans = Math.floor((start + end) / 2);
+        if (input[ans] == target) {
+            return ans;
+        }
+        if (input[ans] > target) {
+            end = ans-1;
+        } else {
+            start = ans+1;
+        }
+    }
+    return -1;
+}
 
 const array = [1, 2, 3, 4, 5, 11, 12, 13, 14, 20];
 
